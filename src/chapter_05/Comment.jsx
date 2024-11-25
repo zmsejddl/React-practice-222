0c3341a3d5a43265f58c30eb03704e690e7f187d
Jung Young Hun <hujung0268@gamil.com>
2024-11-25 13:30:52 +0900

    댓글 컴포넌트 만들기

--- src/chapter_05/Comment.jsx
+++ src/chapter_05/Comment.jsx
@@ -1,9 +1,48 @@
 import React from "react";
 
+const styles = {
+  wrapper: {
+    margin: 8,
+    padding: 8,
+    display: "flex",
+    flexDirection: "row",
+    border: "1px solid grey",
+    borderRadius: 16,
+  },
+  imageContainer: {},
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  contentContainer: {
+    marginLeft: 8,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  nameText: {
+    color: "black",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  commentText: {
+    color: "black",
+    fontSize: 16,
+  },
+};
+
 function Comment(props) {
   return (
-    <div>
-      <h1>제가 만든 첫 컴포넌트입니다.</h1>
+    <div style={styles.wrapper}>
+      <div style={styles.imageContainer}>
+        <img src="" style={styles.image} />
+      </div>
+
+      <div style={styles.contentContainer}>
+        <span style={styles.nameText}>{props.name}</span>
+        <span style={styles.commentText}>{props.comment}</span>
+      </div>
     </div>
   );
 }
