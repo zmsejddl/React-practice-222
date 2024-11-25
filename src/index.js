46801ab1a47da3ae36a192348e2a1b414008769b
Jung Young Hun <hujung0268@gamil.com>
2024-11-25 13:18:34 +0900

    댓글 컴포넌트 만들기

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import reportWebVitals from "./reportWebVitals";
 
 import Library from "./chapter_03/Library";
 import Clock from "./chapter_04/Clock";
+import CommentList from "./chapter_05/CommentList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Clock />
+    <CommentList />
   </React.StrictMode>
 );
 
