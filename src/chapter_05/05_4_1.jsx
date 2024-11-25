ed92042f62ab4ba40ec3077705357efb6670a4e6
Jung Young Hun <hujung0268@gamil.com>
2024-11-25 13:08:03 +0900

    컴포넌트 합성

---
+++ src/chapter_05/05_4_1.jsx
@@ -0,0 +1,15 @@
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App(props) {
+  return (
+    <div>
+      <Welcome name="Mike" />
+      <Welcome name="Steve" />
+      <Welcome name="Jane" />
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
