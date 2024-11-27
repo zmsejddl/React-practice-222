5e1ca06d3f70e886bd7c6f6bb75078c2ec02f316
Jung Young Hun <hujung0268@gamil.com>
2024-11-27 11:54:06 +0900

    useState

---
+++ src/chapter_07/07_2_2.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function Counter(props) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>총 {count}번 클릭했습니다.</p>
+      <button onClick={() => setCount(count + 1)}>클릭</button>
+    </div>
+  );
+}
