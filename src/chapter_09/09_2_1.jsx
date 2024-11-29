aa7e8fb27ed513067a2eed9977db16bba4a5d336
Jung Young Hun <hujung0268@gamil.com>
2024-11-29 10:26:51 +0900

    엘리먼트 변수

---
+++ src/chapter_09/09_2_1.jsx
@@ -0,0 +1,7 @@
+function LoginButton(props) {
+  return <button onClick={props.onClick}>로그인</button>;
+}
+
+function LogoutButton(props) {
+  return <button onClick={props.onClick}>로그아웃</button>;
+}
---
+++ src/chapter_09/09_2_2.jsx
@@ -0,0 +1,25 @@
+function LoginControl(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogoutClick = () => {
+    setIsLoggedIn(false);
+  };
+
+  let button;
+  if (isLoggedIn) {
+    button = <LogoutButton onClick={handleLogoutClick} />;
+  } else {
+    button = <LoginButton onClick={handleLoginClick} />;
+  }
+
+  return (
+    <div>
+      <Greeting isLoggedIn={isLoggedIn} />
+      {button}
+    </div>
+  );
+}
