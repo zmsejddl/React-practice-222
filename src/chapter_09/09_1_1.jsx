014f2501ebedbf149bdeb6ffa4a21796a914795d
Jung Young Hun <hujung0268@gamil.com>
2024-11-29 10:22:40 +0900

    조건부 렌더링이란?

---
+++ src/chapter_09/09_1_1.jsx
@@ -0,0 +1,7 @@
+function UserGreeting(props) {
+  return <h1>다시 오셨군요!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>회원가입을 해 주세요.</h1>;
+}
---
+++ src/chapter_09/09_1_2.jsx
@@ -0,0 +1,7 @@
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
