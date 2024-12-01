379ad236101790b20fba21a10e2a8bb04722af69
Jung Young Hun <hujung0268@gamil.com>
2024-12-01 11:47:25 +0900

    textarea 태그

---
+++ src/chapter_11/11_3_1.jsx
@@ -0,0 +1,22 @@
+function RequestFomr(props) {
+  const [value, setValue] = useState("요청사항을 입력하세요.");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    alert("입력한 요청사항: " + value);
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        요청사항:
+        <textarea value={value} onChange={handleChange} />
+      </label>
+      <button type="submit">제출</button>
+    </form>
+  );
+}
